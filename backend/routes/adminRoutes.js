8aa8432b13478731550d35abcff88194d496e330
Shasank Reddy <shasankreddy4563@gmail.com>
2025-09-02 23:56:30 +0530

    Trigger

--- backend/routes/adminRoutes.js
+++ backend/routes/adminRoutes.js
@@ -3,7 +3,6 @@ const router = express.Router();
 const { addQuestion } = require("../controllers/adminController");
 const authMiddleware = require("../middleware/authMiddleware");
 
-// Suggestion: keep route naming RESTful
 router.post("/questions", authMiddleware, addQuestion);
 
 module.exports = router;
