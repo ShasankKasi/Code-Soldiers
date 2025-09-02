eb2f8de0bf687cc0381047b109dad41a7f87c47e
Shasank Reddy <shasankreddy4563@gmail.com>
2025-09-02 23:28:04 +0530

    Added nginx reverse proxy

--- frontend/src/utils/axiosHelper.jsx
+++ frontend/src/utils/axiosHelper.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
   // eslint-disable-next-line no-undef
-  baseURL: "http://localhost:5000",
+  baseURL: "/api",
 });
 
 // Add JWT to headers automatically
