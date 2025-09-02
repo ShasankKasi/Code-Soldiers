15bfce2e62f61f08882ba7f6098a6be36e87ffa0
Shasank Reddy <113813427+ShasankKasi@users.noreply.github.com>
2025-09-02 11:58:52 +0530

    Update axiosHelper.jsx

--- frontend/src/utils/axiosHelper.jsx
+++ frontend/src/utils/axiosHelper.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
   // eslint-disable-next-line no-undef
-  baseURL: process.env.REACT_APP_API_URL,
+  baseURL: process.env.REACT_APP_API_URL0,
 });
 
 // Add JWT to headers automatically
