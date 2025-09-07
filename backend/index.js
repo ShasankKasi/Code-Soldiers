d9bd0066ce238bfa6186e34a90ff1c5ae3e20e6c
Shasank Reddy <shasankreddy4563@gmail.com>
2025-09-07 16:04:29 +0530

    Debug

--- backend/index.js
+++ backend/index.js
@@ -37,4 +37,6 @@ app.use((err, req, res, next) => {
 });
 
 // Start server
-app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`🚀 Server running on port ${PORT}`);
+});
