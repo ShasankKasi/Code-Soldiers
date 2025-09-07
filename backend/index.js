599cb6238fa18124c2b3ae879f941a868a7a00e9
Shasank Reddy <shasankreddy4563@gmail.com>
2025-09-07 15:27:55 +0530

    Update

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,7 @@ dotenv.config();
 connectDatabase();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 
 // Middleware
 // index.js in backend
