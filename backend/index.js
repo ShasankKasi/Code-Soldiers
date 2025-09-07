e21866b6e1ca71ec667b16204c9610f9f5319523
Shasank Reddy <shasankreddy4563@gmail.com>
2025-09-07 15:22:15 +0530

    Update

--- backend/index.js
+++ backend/index.js
@@ -13,21 +13,16 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-const allowedOrigins = [
-  process.env.frontend_url, // production
-  "http://localhost:5173" // keep for local dev
-];
+// index.js in backend
+const corsOptions = {
+  origin: [
+    process.env.frontend_url,
+    process.env.frontend_url_2,
+  ],
+  credentials: true, // if using cookies
+};
+app.use(cors(corsOptions));
 
-app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
-  credentials: true, // allow cookies/headers if needed
-}));
 app.use(express.json()); 
 app.use(express.urlencoded({ extended: true })); 
 
